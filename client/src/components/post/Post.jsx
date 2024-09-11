aadca9e1ba166d73b28bdc13d388c8e9c10ce108
trqlong26 <trqlong26@gmail.com>
2024-09-11 21:21:24 +0700

    deploy demo version

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -17,7 +17,7 @@ const Post = ({ post }) => {
     const { data: authUser } = useQuery({ queryKey: ["authUser"] });
     const queryClient = useQueryClient();
     const postOwner = post.user;
-    const isLiked = post.likes.includes(authUser._id);
+    const isLiked = Array.isArray(post.likes) && post.likes.includes(authUser._id);
 
     const isMyPost = authUser._id === post.user._id;
 
@@ -49,35 +49,35 @@ const Post = ({ post }) => {
         mutationFn: async () => {
             try {
                 const res = await fetch(`/api/post/like/${post._id}`, {
-                    method: "POST",
-                });
-                const data = await res.json();
+                    method: "POST"
+                })
+
+                const data = await res.json()
+
                 if (!res.ok) {
-                    throw new Error(data.error || "Something went wrong");
+                    throw new Error(data.error || "Something went wrong")
                 }
-                return data;
+
+                return data
             } catch (error) {
-                throw new Error(error);
+                throw new Error(error)
             }
         },
         onSuccess: (updatedLikes) => {
-            // this is not the best UX, bc it will refetch all posts
-            // queryClient.invalidateQueries({ queryKey: ["posts"] });
-
-            // instead, update the cache directly for that post
+            // queryClient.invalidateQueries({ queryKey: ["posts"] })
             queryClient.setQueryData(["posts"], (oldData) => {
                 return oldData.map((p) => {
                     if (p._id === post._id) {
-                        return { ...p, likes: updatedLikes };
+                        return { ...p, likes: updatedLikes }
                     }
-                    return p;
-                });
-            });
+                    return p
+                })
+            })
         },
         onError: (error) => {
-            toast.error(error.message);
-        },
-    });
+            toast.error(error.message)
+        }
+    })
 
     const { mutate: commentPost, isPending: isCommenting } = useMutation({
         mutationFn: async () => {
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import notificationRoute from "./routes/notification.route.js";
 import connectDatabase from "./utils/connectDatabase.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
 
 dotenv.config();
 cloudinary.config({
@@ -17,6 +18,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -26,6 +28,14 @@ app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
 app.use("/api/notification", notificationRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is up and running!`);
